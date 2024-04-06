9784832d1dbb66ef4895b51cb2422d672e4a880d
Rocío <tj.rocio.cruz.z@upds.net.bo>
2024-04-06 00:21:28 -0400

    RJCZ

---
+++ Actividad5/Actividad5.VISTA/Actividad5VISTAS/DetalleVenta.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Actividad5.VISTA.Actividad5VISTAS
+{
+    partial class DetalleVenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            button5 = new Button();
+            button6 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(743, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(29, 87);
+            button1.TabIndex = 8;
+            button1.Text = "D\r\nA\r\nT\r\nO\r\nS";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(175, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Size = new Size(434, 155);
+            dataGridView1.TabIndex = 7;
+            dataGridView1.Click += dataGridView1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(616, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(38, 122);
+            button2.TabIndex = 12;
+            button2.Text = "I\r\nN\r\nS\r\nE\r\nR\r\nT\r\nA\r\nR\r\n";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(660, 12);
+            button3.Name = "button3";
+            button3.Size = new Size(34, 113);
+            button3.TabIndex = 13;
+            button3.Text = "E\r\nD\r\nI\r\nT\r\nA\r\nR";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(700, 12);
+            button4.Name = "button4";
+            button4.Size = new Size(37, 134);
+            button4.TabIndex = 14;
+            button4.Text = "E\r\nL\r\nI\r\nM\r\nI\r\nN\r\nA\r\nR";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(62, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(53, 23);
+            textBox1.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(62, 41);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(53, 23);
+            textBox2.TabIndex = 16;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(60, 70);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(55, 23);
+            textBox3.TabIndex = 17;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(60, 99);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(82, 23);
+            textBox4.TabIndex = 18;
+            textBox4.TextChanged += textBox4_TextChanged;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(60, 128);
+            textBox5.Name = "textBox5";
+            textBox5.ReadOnly = true;
+            textBox5.Size = new Size(82, 23);
+            textBox5.TabIndex = 19;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(121, 12);
+            button5.Name = "button5";
+            button5.Size = new Size(48, 23);
+            button5.TabIndex = 20;
+            button5.Text = "selec";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(121, 41);
+            button6.Name = "button6";
+            button6.Size = new Size(48, 23);
+            button6.TabIndex = 21;
+            button6.Text = "selec";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(18, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 15);
+            label1.TabIndex = 22;
+            label1.Text = "IDVenta";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(18, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 15);
+            label2.TabIndex = 23;
+            label2.Text = "IDProd";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(22, 76);
+            label3.Name = "label3";
+            label3.Size = new Size(32, 15);
+            label3.TabIndex = 24;
+            label3.Text = "Cant";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(-1, 107);
+            label4.Name = "label4";
+            label4.Size = new Size(55, 15);
+            label4.TabIndex = 25;
+            label4.Text = "PriceUnit";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(15, 131);
+            label5.Name = "label5";
+            label5.Size = new Size(39, 15);
+            label5.TabIndex = 26;
+            label5.Text = "TOTAL";
+            // 
+            // DetalleVenta
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(779, 185);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Name = "DetalleVenta";
+            Text = "Detalle Venta";
+            Load += DetalleVentaInterfaz_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private DataGridView dataGridView1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Button button5;
+        private Button button6;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+    }
+}
---
+++ Actividad5/Actividad5.VISTA/Actividad5VISTAS/ProductoSelec.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Actividad5.VISTA.Actividad5VISTAS
+{
+    partial class ProductoSelec
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.DialogResult = DialogResult.OK;
+            button1.Location = new Point(382, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(32, 174);
+            button1.TabIndex = 8;
+            button1.Text = "S\r\nE\r\nL\r\nE\r\nC\r\nC\r\nI\r\nO\r\nN\r\nA\r\nR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Size = new Size(364, 174);
+            dataGridView1.TabIndex = 7;
+            // 
+            // ProductoSelec
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(427, 199);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Name = "ProductoSelec";
+            Text = "Productos";
+            Load += ProductoSeleccionar_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private DataGridView dataGridView1;
+    }
+}
